fbaf2f3c0d3a1cc06319034f53ff6d2d79e7c574
JailsonAlm <jailson.07almeida@gmail.com>
2021-11-28 10:18:35 -0300

    Controller e Repository

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,15 +1,34 @@
 package one.digitalinnovation.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people") // caminho de acesso principal da API
 public class PersonController {
 
-    @GetMapping // Uma operação Http do tipo Get
-    public String getBook(){
-        return "API Test!";
+    private PersonRepository personRepository;
+
+    // Uma das grandes vantagens do Spring é a gestão de dependências
+
+    @Autowired //indica para a string injetar uma aplicação do tipo repository aqui para dentro
+    // A vantagem de inserir dentro de um construtor é que ele facilita para criar testes unitários
+    // mais para frente
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    // @GetMapping - Uma operação Http do tipo Get
+    @PostMapping // Criação de um livro
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        //@RequestBody a firma que está vindo uma requisição do tipo pessoas aqui
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID" +savedPerson.getId())
+                .build();
     }
 }
